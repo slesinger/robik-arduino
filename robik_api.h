c3e9b0c2dd36ae5bcc1eab4be27f275a4fc371e7
Robik <honza@robik>
2015-05-30 06:18:26 +0000

    migration from svn

---
+++ robik_api.h
@@ -0,0 +1,67 @@
+/*
+ * robik_api.h
+ *
+ *  Created on: Jun 28, 2013
+ *      Author: honza
+ * This header file is shared between robik_driver and arduino sketch
+ */
+
+#ifndef ROBIK_API_H_
+#define ROBIK_API_H_
+
+#define OPERATION_HEAD_POSE 1
+#define OPERATION_SET_PARKING_PHASE 2
+#define OPERATION_SET_LED 3
+#define OPERATION_SET_DPIN 4
+#define OPERATION_SET_ARMPOWER 5
+
+#define ARM_CANON_MIN 1
+#define ARM_CANON_MAX 1000
+
+//Hodnoty PWM
+#define ARM_MIN_CLAMP 1200
+#define ARM_MAX_CLAMP 650
+#define ARM_MIN_ROLL 2250
+#define ARM_MAX_ROLL 730
+#define ARM_MIN_ELBOW 2250
+#define ARM_MAX_ELBOW 500
+#define ARM_MIN_SHOULDER 950
+#define ARM_MAX_SHOULDER 1830
+#define ARM_MIN_YAW 2130
+#define ARM_MAX_YAW 600
+
+//Hodnoty Resistoru (OHM)
+#define ARM_RES_MIN_CLAMP 223
+#define ARM_RES_MAX_CLAMP 122
+#define ARM_RES_MIN_ROLL 378
+#define ARM_RES_MAX_ROLL 118
+#define ARM_RES_MIN_ELBOW 545
+#define ARM_RES_MAX_ELBOW 44
+#define ARM_RES_MIN_SHOULDER 170
+#define ARM_RES_MAX_SHOULDER 423
+#define ARM_RES_MIN_SHOULDER2 1
+#define ARM_RES_MAX_SHOULDER2 1024
+#define ARM_RES_MIN_YAW 424
+#define ARM_RES_MAX_YAW 111
+
+//Hodnoty degrees
+#define ARM_DEG_MIN_CLAMP 0		//0.0
+#define ARM_DEG_MAX_CLAMP 40		//0.052 RAD
+#define ARM_DEG_MIN_ROLL -90		//-1.571
+#define ARM_DEG_MAX_ROLL 90		//1.571
+#define ARM_DEG_MIN_ELBOW -110		//-2.094
+#define ARM_DEG_MAX_ELBOW 40		//0.611
+#define ARM_DEG_MIN_SHOULDER 0		//-0.20
+#define ARM_DEG_MAX_SHOULDER 120	//1.7
+#define ARM_DEG_MIN_YAW -107		//-2.05
+#define ARM_DEG_MAX_YAW 63		//1.092
+
+//Menu controls
+#define MASK_MENU_MENU 1
+#define MASK_MENU_OK 2
+#define MASK_MENU_CANCEL 4
+#define MASK_MENU_UP 8
+#define MASK_MENU_DOWN 16
+
+
+#endif /* ROBIK_API_H_ */
---
+++ robot_config.h
@@ -0,0 +1,86 @@
+#ifndef ROBOT_CONFIG
+#define ROBOT_CONFIG
+//==============
+
+/* Bumper */
+#define PIN_BUMPER 39
+
+/* Menu controls */
+#define PIN_MENU_MENU 49
+#define PIN_MENU_OK 51
+#define PIN_MENU_CANCEL 50
+#define PIN_MENU_UP 53
+#define PIN_MENU_DOWN 52
+
+/* Motors */
+#define PIN_MOTOR_LEFT_ENA 2
+#define PIN_MOTOR_LEFT_0 26
+#define PIN_MOTOR_LEFT_1 27
+
+#define PIN_MOTOR_RIGHT_ENA 3
+#define PIN_MOTOR_RIGHT_0 28
+#define PIN_MOTOR_RIGHT_1 29
+
+#define MOTOR_MIN 2
+#define MOTOR_MAX 199 //verified that this is highest speed
+//#define MOTOR_MIN_ROTATION 8/60 //[rot/sec]
+//#define MOTOR_MAX_ROTATION 80/60 //[rot/sec]
+
+#define WHEEL_DIA_MM 100
+#define TICK_LENGTH_MM 1.9 //2.456
+#define AXIAL_LENGTH_MM 305.0
+
+#define MAX_VEL 0.4 // m/s
+#define MAX_TH 2.4  //RAD/s - if one wheel is stopeed and other works full speed
+#define MIN_TH 0
+
+/* Sonar */
+#define PIN_USOUND_ECHO 42
+#define PIN_USOUND_TRIG 43
+#define SONAR_NUM     1  // Number of sensors.
+#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
+#define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+
+/* Parking Light Sensor */
+#define PIN_PARK_SENS_INNER A6
+#define PIN_PARK_SENS_OUTER A7
+
+/* Head */
+#define PIN_MOTION_DETECTOR 38
+#define PIN_LED 40
+
+/* Oko */
+#define PIN_OKO_SERVO_YAW 8
+#define PIN_OKO_SERVO_PITCH 9
+
+/* Odometry encoder */
+#define PIN_ODOM_LEFT 18
+#define PIN_ODOM_RIGHT 19
+#define INT_ODOM_LEFT 5
+#define INT_ODOM_RIGHT 4
+
+/* Power */
+#define PIN_RELAY 30
+#define PIN_PETRPOWER 22
+#define PIN_JETSONPOWER 23
+#define PIN_CHARGER_BUTTON 24  //is set at action_move.py
+//==============
+
+/* Arm */
+#define PIN_SERVO_SENSE_YAW A1
+#define PIN_SERVO_SENSE_SHOULDER A2
+#define PIN_SERVO_SENSE_ELBOW A3
+#define PIN_SERVO_SENSE_ROLL A4
+#define PIN_SERVO_SENSE_CLAMP A5
+#define SERVO_SENSE_INTERVAL 1
+
+#define PIN_ARM_FORE_CLAMP 36
+#define PIN_ARM_BACK_CLAMP 37
+
+//!!!! nastavit jestli se to nehodi na desce. Jinak zjisteno, ze tyto piny jsou volne.
+#define PIN_SERVO_SENSE_SHOULDER2 A8 
+#define PIN_MOTOR_SHOULDER_0 44
+#define PIN_MOTOR_SHOULDER_1 45
+
+
+#endif
